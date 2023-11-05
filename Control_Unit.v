c9bed02993b8d2315441fa12e395506b6c8a45ff
AF3310 <88600358+AF3310@users.noreply.github.com>
2023-11-05 19:12:12 +0200

    Update Control_Unit.v

--- Control_Unit.v
+++ Control_Unit.v
@@ -23,7 +23,7 @@
 
 module Control_Unit #(N=32)(
 input [N-1:0] Instruction,
-output reg Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite,JSel, MuxRFSel, //do we need MUXRFSel
+output reg Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite,AUIPCSel, MuxRFSel, //do we need MUXRFSel
 output reg [1:0] ALUOp, SaveMethod
     );
         always@(*) begin
@@ -38,7 +38,7 @@ output reg [1:0] ALUOp, SaveMethod
                     RegWrite = 1'b1;
                     SaveMethod = 2'b00;
 		            MuxRFSel=1'b1; 
-		            JSel=1'b1;
+		            AUIPCSel=1'b0;
 
                 end
                 
@@ -51,8 +51,7 @@ output reg [1:0] ALUOp, SaveMethod
                     RegWrite = 1'b1;
                     SaveMethod = 2'b00;
                     MuxRFSel=1'b1; 
-                    JSel=1'b1;
-                    MemtoReg=1'b0;
+                    AUIPCSel=1'b0;
                   
                 end
                 
@@ -66,7 +65,7 @@ output reg [1:0] ALUOp, SaveMethod
                     RegWrite = 1'b1;
                     SaveMethod = 2'b00;
 		            MuxRFSel=1'b1; 
-		            JSel=1'b1; //THIS would have to be 1 to select the line coming From writeData(from Mux of memory and ALU_Out)
+		            AUIPCSel=1'b0;
                 end
                 
                 `OPCODE_Store: begin //store
@@ -77,9 +76,7 @@ output reg [1:0] ALUOp, SaveMethod
                     ALUSrc = 1'b1;
                     RegWrite = 1'b0;
 		            MuxRFSel=1'b1;  //X
-		            JSel=1'b1; //X
-		            MemtoReg=1'b0;
-		          
+		            AUIPCSel=1'b0; //X
 		            case(Instruction[`IR_funct3])
                         `SB: begin
                             SaveMethod = 2'b00;
@@ -102,8 +99,7 @@ output reg [1:0] ALUOp, SaveMethod
                     RegWrite = 1'b0;
                     SaveMethod = 2'b00;
 		            MuxRFSel=1'b1; 
-		            JSel=1'b0; //XXX here doesnt matter again 
-		            MemtoReg=1'b0;
+		            AUIPCSel=1'b0; //XXX
 		            
 
                 end
@@ -117,8 +113,7 @@ output reg [1:0] ALUOp, SaveMethod
                    RegWrite=1'b1; //because we will write back to RF
                     //add here signals to control the new Muxes. 
                    MuxRFSel=1'b0; //chooses sel line of branched  
-                   JSel=1'b1; 
-                   MemtoReg=1'b0;
+                   AUIPCSel=1'b1;
                    SaveMethod = 2'b00;
                 end
 
